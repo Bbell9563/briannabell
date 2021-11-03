119d95f64daab109a19ff0eace37142c5223f6be
Brianna Bell <bbell9563@gmail.com>
2021-11-02 20:20:24 -0600

    styled and formatted

--- js-birthday/js/script.js
+++ js-birthday/js/script.js
@@ -1,27 +1,37 @@
+function getInfo() {
+  var userName = window.prompt("Please enter your name", "name")
+  var age = window.prompt("Please enter your age", 0)
+  var birthMonth = window.prompt("Please enter birth month number", 0)
+  var birthStones = {
+    1: "Garnet", 2: "Amethyst", 3: "Aquamarine", 4: "Diamond", 5: "Emerald",
+    6: "Alexandrite", 7: "Ruby", 8: "Peridot", 9: "Sapphire", 10: "Tourmaline",
+    11: "Topaz", 12: "Blue Topaz"
+  }
 
-var userName = window.prompt("Please enter your name", "name")
-var age = window.prompt("Please enter your age", 0)
-var birthMonth = window.prompt("Please enter birth month number", 0)
-var birthStones = {
-  1: "Garnet", 2: "Amethyst", 3: "Aquamarine", 4:"Diamond",5:"Emerald" ,
-  6: "Alexandrite", 7: "Ruby", 8: "Peridot", 9: "Sapphire", 10:"Tourmaline",
-  11: "Topaz", 12: "Blue Topaz"
+  let birthStone = getBirthstone(birthMonth, birthStones)
+
+
+  if (age < 50) {
+    alert(`Hello ${userName}. You are ${age} years old. You are still young! Your birthstone is ${birthStone}`)
+  }
+  else {
+    alert(`Hello ${userName}. You are ${age} years old. Dang! You are old!Your birthstone is ${birthStone}`)
+  }
+  document.getElementById("info").style = "display: block;padding:20px;"
+  document.getElementById("enter-info").style = "display: none;"
+  document.getElementById("user").innerHTML = `Name: ${userName}`
+  document.getElementById("age").innerHTML = `Age: ${age}`
+  document.getElementById("month").innerHTML = `Birth Month: ${birthMonth}`
+  document.getElementById("birth-stone").innerHTML = `Birth Stone: ${birthStone}`
+  document.getElementById("final").innerHTML = `Final message: "Hello ${userName}. You are ${age} years old. Dang! You are old!Your birthstone is ${birthStone}"`
 }
 
-let birthStone = getBirthstone(birthMonth, birthStones)
 
 
-if(age < 50){
-  alert(`Hello ${userName}. You are ${age} years old. You are still young! Your birthstone is ${birthStone}`)
-  
-}
-else{
-  alert(`Hello ${userName}. You are ${age} years old. Dang! You are old!Your birthstone is ${birthStone}`)
-}
 
-function getBirthstone(birthMonth, birthStones){
-  for(var key in birthStones){
-    if(key == birthMonth){
+function getBirthstone(birthMonth, birthStones) {
+  for (var key in birthStones) {
+    if (key == birthMonth) {
       return birthStones[key]
     }
   }
