f321a799b42414a527fee3215a55735a22aba234
Brianna Bell <bbell9563@gmail.com>
2021-11-02 19:43:26 -0600

    added js-birthday

---
+++ js-birthday/js/script.js
@@ -0,0 +1,14 @@
+
+var userName = window.prompt("Please enter your name", "name")
+var age = window.prompt("Please enter your age", 0)
+
+
+
+if(age < 50){
+  alert(`Hello ${userName}. You are ${age} years old. You are still young`)
+}
+else{
+  alert(`Hello ${userName}. You are ${age} years old. Dang! You are old!`)
+}
+
+
